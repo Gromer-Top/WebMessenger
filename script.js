3e2b8b2dbd98231ee1ac72e3aed44d59a4deb37a
Gromer <rus.solovey.09@mail.ru>
2024-12-31 18:47:03 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello");
+function sendMessage () {
+  var chat = document.querySelector("#messenger");
+  var message = document.createElement('DIV');
+  message.classList.add('user-message');
+  message.innerHTML = document.getElementById('input-message__input').value;
+  chat.appendChild(message);
+}
